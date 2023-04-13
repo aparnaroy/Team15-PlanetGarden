f9d63d50e1b32b733fd94f99fbb27259c5b0932f
Emma Frampton <eframpt@udel.edu>
2023-04-13 16:43:07 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,10 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
+            <h5>
+                Emilie Barniak, Jillian Camp, Emma Frampton, Mikaylla Haskins,
+                Aparna Roy
+            </h5>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
