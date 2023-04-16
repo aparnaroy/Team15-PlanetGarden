07a7c1d20caace59ba00ac7a1b1d9dff95e9596a
Jillian Camp <jillsue@udel.edu>
2023-04-15 21:40:27 -0400

    Loaded assets into src folder, edited App.tsx to practice loading images into flexbox containers

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,78 @@
 import React from "react";
 import "./App.css";
+import PlanetGarden from "./Planet-Garden.png";
 import { DisplayTrees } from "./components";
+import { Col, Row } from "react-bootstrap";
+import Oaktree from "./oaktree.png";
+import Spruce from "./spruce-tree.png";
+import Sequoia from "./Sequoia.png";
+import Cedar from "./cedar-tree.png";
+import Larch from "./larch-tree.png";
 
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
-            </header>
+            <header className="App-header">Planet Garden🪴</header>
+            <img
+                src={PlanetGarden}
+                alt="The garden"
+                width="1320"
+                height="580"
+            ></img>
+            <div>
+                <h1>*DROP DOWN COMPONENT*</h1>
+            </div>
+            <header className="App-header2">Available Items</header>
+            <div className="flex-container">
+                <div>
+                    <img
+                        src={Oaktree}
+                        alt="Oak tree"
+                        width="270"
+                        height="270"
+                    ></img>
+                </div>
+                <div>
+                    <img
+                        src={Spruce}
+                        alt="Oak tree"
+                        width="270"
+                        height="270"
+                    ></img>
+                </div>
+                <div>
+                    <img
+                        src={Sequoia}
+                        alt="Oak tree"
+                        width="270"
+                        height="270"
+                    ></img>
+                </div>
+                <div>
+                    <img
+                        src={Cedar}
+                        alt="Oak tree"
+                        width="270"
+                        height="270"
+                    ></img>
+                </div>
+                <div>
+                    <img
+                        src={Larch}
+                        alt="Oak tree"
+                        width="270"
+                        height="270"
+                    ></img>
+                </div>
+            </div>
+            <div className="flex-container">
+                <div>Rose </div>
+                <div>Daisy </div>
+                <div>Lilly </div>
+                <div>Orchid </div>
+                <div>Tulip </div>
+            </div>
+            <Col> COLUMN </Col>
             <h5>
                 Emilie Barniak, Jillian Camp, Emma Frampton, Mikaylla Haskins,
                 Aparna Roy
