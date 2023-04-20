7026d795fb5d004ed09a0d3c7660f8b09bba0080
Emilie <emiliea@udel.edu>
2023-04-19 21:55:14 -0400

    working on displaying elements with text underneath

--- src/components/components.tsx
+++ src/components/components.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Tree } from "../interfaces";
 //import { Button, Container, Row } from "react-bootstrap";
 import Oaktree from "../assets/oaktree.png";
+//import "./App.css";
 
 // make this a display function, display all trees of a list of them
 export function DisplayTrees(): JSX.Element {
@@ -31,36 +32,37 @@ export function DisplayTrees(): JSX.Element {
     };
     const allTrees: Tree[] = [oakTree, spruceTree, mapleTree];
     return (
-        <div>
-            {allTrees.map((aTree: Tree) => {
-                return (
-                    <div key={aTree.name}>
-                        <div style={{ textAlign: "center" }}>
-                            <img
-                                src={Oaktree}
-                                alt="Oak tree"
-                                width="270"
-                                height="270"
-                            ></img>
-                            <br></br>
-                            <h3>{aTree.name}</h3>
-                            <span>{aTree.description}</span>
-                            <br></br>
-                            <span>Quantity: {aTree.quantity}</span>
-                            <br></br>
-                            <span>
-                                Maintenance level: {aTree.maintenance} out of 5
-                            </span>
-                            <br></br>
-                            <span>Price: ${aTree.price}</span>
-                            <br></br>
-                            <span>Rating: {aTree.rating} out of 5</span>
-                            <br></br>
+        <div className="parent-container">
+            <div className="flex-container">
+                {allTrees.map((aTree: Tree) => {
+                    return (
+                        <div key={aTree.name}>
+                            <div style={{ textAlign: "center" }}>
+                                <img
+                                    src={Oaktree}
+                                    alt="Oak tree"
+                                    width="270"
+                                    height="270"
+                                ></img>
+                                <br></br>
+                                <h3>{aTree.name}</h3>
+                                <span>{aTree.description}</span>
+                                <br></br>
+                                <span>Quantity: {aTree.quantity}</span>
+                                <br></br>
+                                <span>
+                                    Maintenance level: {aTree.maintenance} out
+                                    of 5
+                                </span>
+                                <br></br>
+                                <span>Price: ${aTree.price}</span>
+                                <br></br>
+                                <span>Rating: {aTree.rating} out of 5</span>
+                            </div>
                         </div>
-                        <div style={{ color: "5px solid light green" }}></div>
-                    </div>
-                );
-            })}
+                    );
+                })}
+            </div>
         </div>
     );
 }
