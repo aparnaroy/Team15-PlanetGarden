ba66cc11ae04483b94d8d0283fc8e310c0a6a9bd
Aparna Roy <aparnar@udel.edu>
2023-04-22 12:13:36 -0400

    Changed home page route

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,7 +14,7 @@ export function Navbar() {
                         height="70px"
                     />
                     <Nav.Link
-                        to="/homepage"
+                        to="/"
                         as={NavLink}
                         style={{
                             display: "flex",
